c6437f384766e61a1197f79fb37c95e5bb83f33a
innerWang <52886377@qq.com>
2021-01-04 14:04:19 +0800

    feat: use experimental feature script setup

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,17 @@
   <router-view />
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import { provide, ref } from 'vue';
 import router from './router';
-export default {
-  name: 'App',
-  setup() {
-    const screenWidth = document.documentElement.clientWidth;
-    const isMobile = screenWidth <= 500;
-    const collapse = ref(isMobile);
-    provide('collapse', collapse);
-    router.afterEach(() => {
-      if (isMobile) {
-        collapse.value = true;
-      }
-    });
-  },
-};
+
+const screenWidth = document.documentElement.clientWidth;
+const isMobile = screenWidth <= 500;
+const collapse = ref(isMobile);
+provide('collapse', collapse);
+router.afterEach(() => {
+  if (isMobile) {
+    collapse.value = true;
+  }
+});
 </script>
